d231274330a12ec32f65f5ef75c8c989f71f545b
Luis Fernando Brasil <luis4756@live.com>
2023-02-14 21:37:06 -0300

    Exercicio 04 Cálculo folha de pagamento

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,6 +1,11 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        FolhaDePagamento folha1 = new FolhaDePagamento(220, 8.50F);
+        int horasTrabalhadas = Integer.parseInt(JOptionPane.showInputDialog("Digite a quantidade de horas trabalhadas"));
+        float valorHora = Float.parseFloat(JOptionPane.showInputDialog("Digite o valor hora no formato 'xx.xx' "));
+
+        FolhaDePagamento folha1 = new FolhaDePagamento(horasTrabalhadas, valorHora);
         folha1.calculaFolhaPagamento();
 
     }
